86934a861d89d704bcac46cf13ad7c80984af42c
melanie Boardman <melanie_boardman@hotmail.com>
2020-07-29 12:58:45 +0100

    added products app, models and fixtures

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -38,11 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
     'home',
+    'products',
 ]
 
 MIDDLEWARE = [
